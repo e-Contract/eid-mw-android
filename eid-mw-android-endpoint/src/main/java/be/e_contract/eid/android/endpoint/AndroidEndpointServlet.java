bbdb168f6d273ec6681fc7542b05b769e6000d93
Frank Cornelis <fcorneli@users.noreply.github.com>
2013-12-29 10:44:17 +0100

    endpoint: get task

--- eid-mw-android-endpoint/src/main/java/be/e_contract/eid/android/endpoint/AndroidEndpointServlet.java
+++ eid-mw-android-endpoint/src/main/java/be/e_contract/eid/android/endpoint/AndroidEndpointServlet.java
@@ -18,10 +18,57 @@
 
 package be.e_contract.eid.android.endpoint;
 
-import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-public class AndroidEndpointServlet extends HttpServlet {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+/**
+ * The server-side eID Middleware for Android endpoint.
+ * 
+ * @author Frank Cornelis
+ * 
+ */
+public abstract class AndroidEndpointServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
+	private static final Log LOG = LogFactory
+			.getLog(AndroidEndpointServlet.class);
+
+	@Override
+	protected void doPost(HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletresponse) throws ServletException,
+			IOException {
+		LOG.debug("doPost");
+		BeIDOperation operation = getOperation();
+		if (BeIDOperation.READ_IDENTITY == operation) {
+			Response response = new Response();
+			response.action = "identity";
+			PrintWriter printWriter = httpServletresponse.getWriter();
+			GsonBuilder builder = new GsonBuilder();
+			Gson gson = builder.create();
+			printWriter.write(gson.toJson(response));
+		}
+	}
+
+	private static class Response {
+		String action;
+	}
+
+	/**
+	 * Gives back the eID operation to be performed.
+	 * 
+	 * @return
+	 */
+	protected abstract BeIDOperation getOperation();
 }
